215549ec4e74df01a3eed3d46dbe2cd126964952
Javier Rotelli <javier.rotelli@gmail.com>
2016-01-31 12:46:43 -0300

    pantalla de fin de juego

--- Assets/scripts/MainMapa.cs
+++ Assets/scripts/MainMapa.cs
@@ -7,11 +7,41 @@ public class MainMapa : MonoBehaviour {
 	Estado estado;
 	int textoActual = 0;
 
+
+
 	GameObject viewportMapa;
+	GameObject pantallaIntro;
+	GameObject fondo;
+	GameObject pantallaFin;
+
+	const string textoPerdiste = "Sacerdote... has fallado.\n\n Pero aún creemos en ti.\n Intentalo nuevamente, eres nuestra unica esperanza";
+	const string textoGanaste = "Sacerdote... has fallado.\n\n Pero aún creemos en ti.\n Intentalo nuevamente, eres nuestra unica esperanza";
+	public Sprite spritePerdiste;
+	public Sprite spriteGanaste;
 
 	void Awake () {
 		estado = Estado.Instance;
 		viewportMapa = GameObject.Find ("ViewportMapa");
+		pantallaIntro = GameObject.Find ("PantallaIntro");
+		pantallaFin = GameObject.Find ("PantallaPerdiste");
+		fondo = GameObject.Find ("Fondo");
+	}
+
+	void Start() {
+		switch (estado.nivelActual) {
+		case 0:
+			pantallaIntro.GetComponent<CanvasGroup> ().alpha = 1;
+			pantallaIntro.GetComponent<PantallaIntro> ().enabled = true;
+			break;
+		case -1:
+			procesarFinal (textoPerdiste, spritePerdiste);
+			break;
+		case 7:
+			procesarFinal (textoGanaste, spriteGanaste);
+			break;
+		default:
+			break;
+		}
 	}
 
 	public void handleClick ()
@@ -38,5 +68,12 @@ public class MainMapa : MonoBehaviour {
 	void pasarABatalla ()
 	{
 		SceneManager.LoadScene ("batalla");
+	}
+
+	void procesarFinal(string texto, Sprite imagen){
+		pantallaFin.GetComponentInChildren<Image> ().sprite = imagen;
+		pantallaFin.GetComponentInChildren<Text> ().text = texto;
+		pantallaIntro.GetComponent<CanvasGroup> ().alpha = 1;
+		pantallaFin.GetComponent<PantallaIntro> ().enabled = true;
 	}
 }
