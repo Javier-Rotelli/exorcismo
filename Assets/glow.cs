181c2c55c085903ca19bccc81c5296188fb7a14c
Javier Rotelli <javier.rotelli@gmail.com>
2016-01-31 14:00:12 -0300

    shader pa ivan

---
+++ Assets/glow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class glow : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Mathf.Abs( Mathf.Cos (Time.time)));
+		var material = gameObject.GetComponent<SpriteRenderer> ().material;
+		material.SetFloat ("ShineLocation", Mathf.Abs( Mathf.Cos (Time.time)));
+
+		MaterialPropertyBlock pb = new MaterialPropertyBlock ();
+		gameObject.GetComponent<SpriteRenderer> ().GetPropertyBlock(pb);
+		pb.SetFloat ("ShineLocation", Mathf.Abs (Mathf.Cos (Time.time)));
+		gameObject.GetComponent<SpriteRenderer> ().SetPropertyBlock (pb);
+	}
+}
