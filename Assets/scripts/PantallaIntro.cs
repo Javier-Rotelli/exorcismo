2e357c30117b39945089b90b13b3696569d6bc84
Javier Rotelli <javier.rotelli@gmail.com>
2016-01-31 00:55:35 -0300

    audio + xml pa' ivan

---
+++ Assets/scripts/PantallaIntro.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class PantallaIntro : MonoBehaviour, IPointerClickHandler {
+
+	protected CanvasGroup cg;
+	protected bool animating = false;
+
+	// Use this for initialization
+	void Start () {
+		cg = GetComponent<CanvasGroup> ();
+	}
+
+	void Update(){
+		if (cg.alpha > 0f && animating) {
+			cg.alpha -= Time.deltaTime;
+		} else if (cg.alpha <= 0f) {
+			animating = false;
+			GameObject.Find ("Musica").GetComponent<AudioSource>().Play ();
+		}
+	}
+
+	#region IPointerClickHandler implementation
+	public void OnPointerClick (PointerEventData eventData)
+	{
+		animating = true;
+	}
+	#endregion
+}
